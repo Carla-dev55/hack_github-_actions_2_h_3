ffda22b03ee5bd2653332973f64cbde5be991a92
Carla-dev55 <arqsivira55@gmail.com>
2026-07-14 17:53:33 -0400

    feat(fix) vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/hack_gh_3/'
+  base: '/hack_github-_actions_2_h_3/'
 })
